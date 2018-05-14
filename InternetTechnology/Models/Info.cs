af3514f7194f06b3fd153dff2275045b5eefa62b
Bergs22 <mads22berggreen@live.dk>
2018-05-14 19:14:01 +0200

    Various renamings + added a String for email

--- InternetTechnology/Models/Info.cs
+++ InternetTechnology/Models/Info.cs
@@ -9,6 +9,7 @@ namespace InternetTechnology.Models
     {
         public int ID { get; set; }
         public String Name { get; set; }
+        public String Email { get; set; }
         public String BirthDay { get; set; }
         public int Age { get; set; }
         public String Occupation { get; set; }
